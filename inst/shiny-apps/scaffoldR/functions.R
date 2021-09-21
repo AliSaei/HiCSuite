9b63d6335e8d76365b10e99910220cc8ff238acf
Ali Saei <Ali.Saei@agresearch.co.nz>
2021-09-21 15:15:01 +1200

    removing plotting from functions

--- inst/shiny-apps/scaffoldR/functions.R
+++ inst/shiny-apps/scaffoldR/functions.R
@@ -1,15 +1,15 @@
-join_maps <- function(mat, seq, subseq, binsize, direction = "Forward", output = "data", output_dir = NULL){
+join_maps <- function(mat, seq, subseq, binsize, direction = "Forward", output_dir = NULL){
   
   if(is.null(seq)) 
     return(NULL)
   
-  if(!is.null(output_dir)){
-    
-    # cretae a dir to store tiff files
-    if(!dir.exists(file.path(output_dir, seq))){
-      dir.create(file.path(output_dir, seq), showWarnings = FALSE, recursive = TRUE)
-    }
-  }
+  #if(!is.null(output_dir)){
+
+  #   # cretae a dir to store tiff files
+  #   if(!dir.exists(file.path(output_dir, seq))){
+  #     dir.create(file.path(output_dir, seq), showWarnings = FALSE, recursive = TRUE)
+  #   }
+  # }
   
   seq <- seq[length(seq)]
   
@@ -41,38 +41,13 @@ join_maps <- function(mat, seq, subseq, binsize, direction = "Forward", output =
                                 mpos = ifelse(mrnm == ref_name[2], max_rf2-mpos, mpos))] %>%
         .[, ':='(pos = ifelse(rname == ref_name[2], (pos + max_ref1), pos),
                  mpos = ifelse(mrnm == ref_name[2], (mpos + max_ref1), mpos))]
-      
     } 
-    
-    if(output == "data"){
-      return(sel_mat)
-    } else {
-      p <- ggplot(sel_mat,aes(x = pos, y = mpos, fill=log10(n/2))) +
-        geom_tile(color = "red", size = 0.2) +
-        #scale_fill_gradient(low = "#ff9999", high = "red") +
-        labs(title = i, x = "", y = "") +
-        scale_x_continuous(expand=c(0,0)) +
-        scale_y_continuous(expand=c(0,0)) +
-        geom_vline(xintercept = max(sel_mat[rname == ref_name[1], pos])	, color = "blue", size = 0.2) +
-        geom_hline(yintercept = max(sel_mat[rname == ref_name[1], pos])	, color = "blue", size = 0.2) +
-        theme(axis.text = element_blank(),
-              axis.ticks = element_blank(), 
-              legend.position = "none",
-              panel.border = element_rect(colour = "gray", fill = NA),
-              panel.background = element_rect(fill = "white", colour = "white"))
-      
-      if(!is.null(output_dir)){
-        filename = paste0(n,i, ".tiff")
-        ggsave(file.path(output_dir, seq, filename), scale = 1, width = 10, height = 10, units = "cm")
-      } else {
-        p
-      }
-    }
   }
+  return(sel_mat)
 }
 
 
-join_maps_plus <- function(mat, seq, subseq = NULL,  binsize, direction = "Forward", output_dir = NULL, output = list("data", "graph")){
+join_maps_plus <- function(mat, seq, subseq = NULL,  binsize, direction = "Forward"){
   # name the chromosme being assembled
   
   
@@ -85,11 +60,12 @@ join_maps_plus <- function(mat, seq, subseq = NULL,  binsize, direction = "Forwa
   
   
   if(n < 3) {
-    combined_maps <- join_maps(mat, maps[1], maps[2], direction = direction , output_dir = output_dir, binsize = binsize, output = output)
+    combined_maps <- join_maps(mat, maps[1], maps[2], direction = direction, binsize = binsize)
   } else {
     if(direction == "Backward") maps <- gsub("^\\s+|\\s$", "", c(subseq, seq))
     
     combined_maps <- join_maps(mat, maps[1], maps[2], binsize = binsize,  output = "data")
+    
     for(i in 3:n) {
       
       # remove '+-" signs
@@ -132,32 +108,6 @@ join_maps_plus <- function(mat, seq, subseq = NULL,  binsize, direction = "Forwa
       }
       combined_maps <- rbindlist(list(combined_maps, sel_mat))
     }
-    
-    if(output == "data"){
-      return(combined_maps)
-    } else {
-      len <- combined_maps[, .(len= max(pos)), by = .(rname)] %>%
-        .[order(len), ]
-      
-      p <- ggplot(combined_maps,aes(x = pos, y = mpos, fill=log10(n))) +
-        geom_tile(color = "red", size = 0.2) +
-        labs(title = paste("Total size:", max(len$len)/1000000, "MB"), x = "", y = "") +
-        scale_x_continuous(expand=c(0,0)) +
-        scale_y_continuous(expand=c(0,0)) +
-        geom_vline(xintercept = len$len	, color = "blue", size = 0.1) +
-        geom_hline(yintercept = len$len	, color = "blue", size = 0.1) +
-        theme(axis.text = element_blank(),
-              axis.ticks = element_blank(), 
-              legend.position = "none",
-              panel.background = element_rect(fill = "white", colour = "white"))
-      
-      if(!is.null(output_dir)){
-        filename = paste0(n,i, ".tiff")
-        ggsave(file.path(output_dir, seq, filename), scale = 1, width = 10, height = 10, units = "cm")
-        return(NULL)
-      } else {
-        p
-      }
-    }
   }
+  return(combined_maps)
 }
